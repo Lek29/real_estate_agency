a0712721b685e123565987a3c2f7afbdb58f373f
Lek29 <ligioner29@mail.ru>
2025-01-15 22:29:17 +0800

    made a flat code

--- property/migrations/0013_normalize_phone_numbers_fix2.py
+++ property/migrations/0013_normalize_phone_numbers_fix2.py
@@ -12,14 +12,17 @@ def normalize_phone_numbers(apps, shema_editor):
         try:
             parsed_number = phonenumbers.parse(flat.owners_phonenumber, "RU")
 
-            if phonenumbers.is_valid_number(parsed_number):
-                normalized_phone_number = phonenumbers.format_number(
-                    parsed_number, PhoneNumberFormat.E164
-                    )
-                flat.owner_pure_phone = normalized_phone_number
-            else:
+            if not phonenumbers.is_valid_number(parsed_number):
                 flat.owner_pure_phone = None
+                flat.save()
+                continue
+
+            normalized_phone_number = phonenumbers.format_number(
+                parsed_number, PhoneNumberFormat.E164
+            )
+            flat.owner_pure_phone = normalized_phone_number
             flat.save()
+
         except phonenumbers.phonenumberutil.NumberParseException:
             flat.owner_pure_phone = None
             flat.save()
